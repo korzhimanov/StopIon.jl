0825b8a7842976c75384cc7be1887e003be42d59
korzhimanov <korzhimanov.artem@gmail.com>
2019-04-08 18:06:30 +0300

    Add Particle struct

--- src/StopIon.jl
+++ src/StopIon.jl
@@ -1,5 +1,12 @@
 module StopIon
-export hello, domath
+using Unitful
+
+export Particle, hello, domath
+
+struct Particle
+    z::Float64
+    m::Unitful.Mass
+end
 
 hello(who::String) = "Hello, $who"
 domath(x::Number) = (x + 5)
